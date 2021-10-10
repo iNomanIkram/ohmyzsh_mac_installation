fb6e9cb27420b7edd9ff1016511d39b21d47c6ff
Noman Ikram <nomanikram0@icloud.com>
2021-10-10 07:52:33 +0500

    initial commit

---
+++ install_ohmyzsh.sh
@@ -0,0 +1,7 @@
+brew install zsh
+brew install romkatv/powerlevel10k/powerlevel10k
+echo "source $(brew --prefix)/opt/powerlevel10k/powerlevel10k.zsh-theme" >>~/.zshrc
+p10k configure
+
+
+brew install --cask iterm2
